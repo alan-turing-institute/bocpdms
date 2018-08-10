2e9e5c90194f96a777e66ef539decf78bd11b132
Louise Bowler <lbowler@turing.ac.uk>
2018-08-10 17:40:07 +0100

    Save all figures and output error metrics.

--- whistler_ICML18.py
+++ whistler_ICML18.py
@@ -10,6 +10,7 @@ Description: Reads in and processes Whistler data
 """
 
 import numpy as np
+import scipy
 import csv, os
 import matplotlib.pyplot as plt
 
@@ -19,10 +20,11 @@ from Evaluation_tool import EvaluationTool
 from cp_probability_model import CpModel
 
 
-baseline_working_directory = os.getcwd()
-whistler_data = os.path.join(baseline_working_directory, "Data", "whistler_data.csv")
-whistler_dates = os.path.join(baseline_working_directory, "Data", "whistler_dates.csv")
-whistler_results = os.path.join(baseline_working_directory, "Output", "results_whistler.txt")
+data_directory = os.path.join(os.getcwd(), "Data")
+output_directory = os.path.join(os.getcwd(), "Output")
+whistler_data = os.path.join(data_directory, "whistler_data.csv")
+whistler_dates = os.path.join(data_directory, "whistler_dates.csv")
+whistler_results = os.path.join(output_directory, "results_whistler.txt")
 
 
 """STEP 0: Decide what to do"""
@@ -30,7 +32,7 @@ run_detectors = True
 save_plots = False
 print_plots = False
 normalize = True
-log_transform=True
+log_transform = True
 
 """STEP 1: Read in and convert to float"""
 raw_data = []
@@ -98,12 +100,12 @@ AR_models += [BVARNIG(prior_a = prior_a,prior_b = prior_b,
             hyperparameter_optimization = hyperpar_opt)]
 
 for lag in range(lower_AR, upper_AR+1):
-    
+
     #Set the prior of the coefficients s.t. the intercept can be different from
     #the lag-coefficient priors.
-    prior_mean_beta = np.array([intercept_scale] + [0]*lag) 
-    
-    
+    prior_mean_beta = np.array([intercept_scale] + [0]*lag)
+
+
     #fit some dynamic parts
     AR_models += [BVARNIG(
                     prior_a = prior_a,prior_b = prior_b,
@@ -114,7 +116,7 @@ for lag in range(lower_AR, upper_AR+1):
                     nbh_sequence = [0]*lag,
                     restriction_sequence = [0]*lag,
                     hyperparameter_optimization = hyperpar_opt)]
-    
+
 
 """model universe and model priors"""
 model_universe = np.array(AR_models)
@@ -122,11 +124,11 @@ model_prior = np.array([1/len(model_universe)]*len(model_universe))
 
 """run detectors, potentially plot stuff"""
 """Build and run detector"""
-detector = Detector(data=data, 
-        model_universe=model_universe, 
+detector = Detector(data=data,
+        model_universe=model_universe,
         model_prior = model_prior,
-        cp_model = cp_model, 
-        S1 = S1, S2 = S2, T = T, 
+        cp_model = cp_model,
+        S1 = S1, S2 = S2, T = T,
         store_rl=True, store_mrl=True,
         trim_type="keep_K", threshold = 200,
         training_period = 25, #i.e., we let 2 years pass before MSE computed
@@ -139,29 +141,33 @@ EvT = EvaluationTool()
 EvT.build_EvaluationTool_via_run_detector(detector)
 EvT.store_results_to_HD(whistler_results)
 
-"""plot transformed data"""
-fig = EvT.plot_raw_TS(data.reshape(T,1))
+"""Plot transformed data"""
+fig, ax = plt.subplots()
+EvT.plot_raw_TS(data.reshape(T, 1), ax=ax)
+fig.savefig(os.path.join(output_directory, "whistler_transformed_data.pdf"), format="pdf", dpi=800)
 
-"""plot prediction error"""
-fig = EvT.plot_prediction_error(data, indices=[0], print_plt=True,
-                                time_range = np.linspace(2*365,
-                             T-upper_AR-1,
-                             T-upper_AR-1-2*365,
-                             dtype=int))
+"""Plot prediction error"""
+fig, ax = plt.subplots()
+EvT.plot_prediction_error(data, indices=[0], ax=ax,
+                          time_range=np.linspace(2*365, T-upper_AR-1, T-upper_AR-1-2*365, dtype=int))
+fig.savefig(os.path.join(output_directory, "whistler_prediction_error.pdf"), format="pdf", dpi=800)
 
-"""plot predictions themselves"""
-fig = EvT.plot_predictions(
-        indices = [0], print_plt = True, 
-        legend = False, 
-        legend_labels = None, 
-        legend_position = None, 
-        time_range = None,
-        show_var = False, 
-        show_CPs = True)
+"""Plot predictions themselves"""
+fig, ax = plt.subplots()
+EvT.plot_predictions(indices=[0],
+                     ax=ax,
+                     legend=False,
+                     legend_labels=None,
+                     legend_position=None,
+                     time_range=None,
+                     show_var=False,
+                     show_CPs=True)
+fig.savefig(os.path.join(output_directory, "whistler_predictions.pdf"), format="pdf", dpi=800)
 
-plt.show()
-
+"""Print results for paper"""
 print("\n")
 print("***** Predictive MSE + NLL from Table 1 in ICML 2018 paper *****")
-print("MSE is %.5g" % (np.sum(np.mean(detector.MSE, axis=0))))
-print("NLL is %.5g" % (np.mean(detector.negative_log_likelihood)))
+print("MSE is %.5g with 95%% error of %.5g" %
+      (np.mean(detector.MSE), 1.96*scipy.stats.sem(detector.MSE)))
+print("NLL is %.5g with 95%% error of %.5g" %
+      (np.mean(detector.negative_log_likelihood), 1.96*scipy.stats.sem(detector.negative_log_likelihood)))
