1c008ed888120d9b3a02e07446e5400e82829918
Louise Bowler <lbowler@turing.ac.uk>
2018-08-20 16:40:09 +0100

    Save the extra figures (previously commented out) if specifically requested, and another small tidy-up.

--- paper_pictures_nileData.py
+++ paper_pictures_nileData.py
@@ -17,6 +17,8 @@ import os
 import matplotlib
 import datetime
 
+# Choose whether to generate and save additional figures (not used in the paper)
+save_extra_figures = False
 
 # Ensure that we have type 1 fonts (for ICML publishing guiedlines)
 matplotlib.rcParams['pdf.fonttype'] = 42
@@ -30,6 +32,7 @@ if not os.path.isfile(results_file):
     print("\nCould not find results_nile.txt in the Output directory. Have you run nile_ICML18.py?\n")
 
 """STEP 1: Read in and convert to float (as was previously done in nile_ICML18.py)"""
+
 raw_data = []
 count = 0 
 with open(nile_file) as csvfile:
@@ -43,6 +46,7 @@ for entry in raw_data:
 raw_data = raw_data_float
 
 """STEP 2: put into right form"""
+
 T = int(len(raw_data)/2)
 S1, S2 = 1,1
 data = np.array(raw_data).reshape(T,2)
@@ -52,20 +56,23 @@ mean, variance = np.mean(river_height), np.var(river_height)
 river_height = (river_height-mean)/np.sqrt(variance)
 
 """STEP 3: Get dates"""
+
 all_dates = []
 for i in range(622+2, 1285):
     all_dates.append(datetime.date(i, 1,1))
 
-"""Read in results"""
+# Read in results
 EvT = EvaluationTool()
 EvT.build_EvaluationTool_via_results(results_file) 
 
-"""get MAP CPs in range"""
+# Get MAP CPs in range
 segmentation = np.array(EvT.results[EvT.names.index("MAP CPs")][-2])
 models = np.union1d([e[1] for e in segmentation],
                     [e[1] for e in segmentation])
 
 """STEP 4: Generate run-length distribution plot for paper"""
+
+# Prepare the axes
 start, stop = (2007 + 7 / 12), 2009
 height_ratio = [10, 14]
 custom_colors = ["blue", "purple"]
@@ -73,14 +80,14 @@ fig, ax_array = plt.subplots(2, figsize=(8, 5), sharex=True,
                              gridspec_kw={'height_ratios': height_ratio})
 plt.subplots_adjust(hspace=.35, left=None, bottom=None, right=None, top=None)
 
-"""Get the date format"""
+# Get the date format
 years = mdates.YearLocator()  # every year
 yearsFmt = mdates.DateFormatter('%Y')
 
-"""Placement of y-labels"""
+# Placement of y-labels
 ylabel_coords = [-0.065, 0.5]
 
-"""Upper panel: plot the time-series"""
+# Upper panel: plot the time-series
 EvT.plot_raw_TS(river_height[2:].reshape(T - 2, 1),
                 indices=[0],
                 xlab=None,
@@ -96,7 +103,7 @@ EvT.plot_raw_TS(river_height[2:].reshape(T - 2, 1),
                 ylabel_coords=ylabel_coords,
                 set_ylims=(-2.75, 3.95))
 
-"""Lower panel: plot the run length distribution"""
+# Lower panel: plot the run length distribution
 EvT.plot_run_length_distr(buffer=0,
                           show_MAP_CPs=True,
                           mark_median=False,
@@ -134,16 +141,29 @@ EvT.plot_run_length_distr(buffer=0,
                           arrow_distance=25)
     
 
-"""Save the generated figures"""
+# Save the generated figures
 fig.savefig(os.path.join(baseline_working_directory, "Output", "ICML18_Figure5_Nile.pdf"),
             format="pdf", dpi=800)
 fig.savefig(os.path.join(baseline_working_directory, "Output", "ICML18_Figure5_Nile.png"),
             format="png", dpi=800)
+if os.path.isfile(os.path.join(baseline_working_directory, "Output", "ICML18_Figure5_Nile.pdf")):
+    print("Figure 5 can be found in the Output directory")
 
-"""STEP 5: Generate additional figures"""
-# EvT.plot_prediction_error(river_height.reshape(T,1,1), indices = [0],
-#                          show_var = True, up_to = 250,
-#                          show_MAP_CPs = True)
+"""STEP 5: Generate additional figures, if requested"""
 
-# EvT.plot_model_posterior(indices = [0,1,2], log_format = False, up_to = 150,
-#                         plot_type = "MAPVariance1_det", show_MAP_CPs = True)
+if save_extra_figures:
+
+    # Plot the prediction error and variance
+    fig_pe, ax_pe = plt.subplots()
+    EvT.plot_prediction_error(river_height.reshape(T, 1, 1), indices=[0],
+                              show_var=True, show_MAP_CPs=True,
+                              up_to=250, ax=ax_pe)
+    fig_pe.savefig(os.path.join(baseline_working_directory, "Output", "ICML18_ExtraFigure_Nile_PredError.pdf"),
+                   format="pdf", dpi=800)
+
+    # Plot the model posterior
+    fig_mp, ax_mp = plt.subplots()
+    EvT.plot_model_posterior(indices=[0, 1, 2], log_format=False, up_to=150,
+                             plot_type="MAPVariance1_det", show_MAP_CPs=True, ax=ax_mp)
+    fig_mp.savefig(os.path.join(baseline_working_directory, "Output", "ICML18_ExtraFigure_Nile_ModelPosterior.pdf"),
+                   format="pdf", dpi=800)
