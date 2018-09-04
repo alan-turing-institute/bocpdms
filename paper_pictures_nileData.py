a769364fb42d826b3cc4c12d507bd6598c4a4f21
Louise Bowler <lbowler@turing.ac.uk>
2018-09-04 14:03:07 +0100

    Tidy up and import data using function defined in nile_ICML18.py.

--- paper_pictures_nileData.py
+++ paper_pictures_nileData.py
@@ -9,18 +9,17 @@ Description: Get the Nile data pics
 """
 
 import numpy as np
-from Evaluation_tool import EvaluationTool
-from matplotlib import pyplot as plt
-import matplotlib.dates as mdates
-import csv
-import os
+import matplotlib.pyplot as plt
 import matplotlib
-import datetime
+import os
+
+from Evaluation_tool import EvaluationTool
+from nile_ICML18 import load_nile_data
 
 # Choose whether to generate and save additional figures (not used in the paper)
 save_extra_figures = False
 
-# Ensure that we have type 1 fonts (for ICML publishing guiedlines)
+# Ensure that we have type 1 fonts (for ICML publishing guidelines)
 matplotlib.rcParams['pdf.fonttype'] = 42
 matplotlib.rcParams['ps.fonttype'] = 42
 
@@ -31,71 +30,40 @@ results_file = os.path.join(baseline_working_directory, "Output", "results_nile.
 if not os.path.isfile(results_file):
     print("\nCould not find results_nile.txt in the Output directory. Have you run nile_ICML18.py?\n")
 
-"""STEP 1: Read in and convert to float (as was previously done in nile_ICML18.py)"""
+"""STEP 1: Read in data and convert to appropriate format"""
 
-raw_data = []
-count = 0 
-with open(nile_file) as csvfile:
-    reader = csv.reader(csvfile)
-    for row in reader:
-        raw_data += row
+# Extract data (height and date) and properties (T: num years; S1 and S2: spatial dimensions) from csv file
+T, S1, S2, river_height, unstandardised_river_height, dates = load_nile_data(nile_file)
 
-raw_data_float = []
-for entry in raw_data:
-    raw_data_float.append(float(entry))
-raw_data = raw_data_float
-
-"""STEP 2: put into right form"""
-
-T = int(len(raw_data)/2)
-S1, S2 = 1,1
-data = np.array(raw_data).reshape(T,2)
-dates = data[:,0]
-river_height = data[:,1]
-mean, variance = np.mean(river_height), np.var(river_height)
-river_height = (river_height-mean)/np.sqrt(variance)
-
-"""STEP 3: Get dates"""
-
-all_dates = []
-for i in range(622+2, 1285):
-    all_dates.append(datetime.date(i, 1,1))
+"""Step 2: Generate EvaluationTool based on the results generated with nile_ICML18.py"""
 
 # Read in results
 EvT = EvaluationTool()
 EvT.build_EvaluationTool_via_results(results_file) 
 
 # Get MAP CPs in range
-segmentation = np.array(EvT.results[EvT.names.index("MAP CPs")][-2])
-models = np.union1d([e[1] for e in segmentation],
-                    [e[1] for e in segmentation])
+print("Identified", len(EvT.results[EvT.names.index("MAP CPs")][-2]), "MAP CPs at years: ")
+[print("\t", int(dates[m[0]])) for m in EvT.results[EvT.names.index("MAP CPs")][-2]]
 
-"""STEP 4: Generate run-length distribution plot for paper"""
+"""STEP 3: Generate run-length distribution plot for paper"""
 
 # Prepare the axes
-start, stop = (2007 + 7 / 12), 2009
-height_ratio = [10, 14]
-custom_colors = ["blue", "purple"]
 fig, ax_array = plt.subplots(2, figsize=(8, 5), sharex=True,
-                             gridspec_kw={'height_ratios': height_ratio})
+                             gridspec_kw={'height_ratios': [10, 14]})
 plt.subplots_adjust(hspace=.35, left=None, bottom=None, right=None, top=None)
-
-# Get the date format
-years = mdates.YearLocator()  # every year
-yearsFmt = mdates.DateFormatter('%Y')
 
 # Placement of y-labels
 ylabel_coords = [-0.065, 0.5]
 
 # Upper panel: plot the time-series
-EvT.plot_raw_TS(river_height[2:].reshape(T - 2, 1),
+EvT.plot_raw_TS(river_height[2:].reshape(T-2, 1),
                 indices=[0],
                 xlab=None,
                 show_MAP_CPs=True,
-                time_range=np.linspace(1, T - 2, T - 2, dtype=int),
+                time_range=np.linspace(1, T-2, T-2, dtype=int),
                 print_plt=False,
                 ylab="River Height", ax=ax_array[0],
-                all_dates=np.linspace(622 + 1, 1284, 1284 - (622 + 1), dtype=int),  # None, #all_dates,
+                all_dates=np.linspace(int(dates[1]), int(dates[-2]), T-2, dtype=int),
                 custom_colors_series=["black"],
                 custom_colors_CPs=["blue", "blue"],
                 custom_linestyles=["solid"] * 2,
@@ -114,13 +82,12 @@ EvT.plot_run_length_distr(buffer=0,
                           log_format=True,
                           aspect_ratio='auto',
                           C1=0, C2=1,
-                          time_range=np.linspace(1, T - 2, T - 2, dtype=int),
-                          start=622 + 2, stop=1284,
-                          all_dates=None,                       # all_dates,
-                          event_time_list=[715],                # datetime.date(715,1,1)],
+                          time_range=np.linspace(1, T-2, T-2, dtype=int),
+                          start=int(dates[2]), stop=int(dates[-1]),
+                          event_time_list=[715],    # Nilometer installed in year 715
                           label_list=["nilometer"],
                           space_to_colorbar=0.52,
-                          custom_colors=["blue", "blue"],       # ["blue"]*len(event_time_list),
+                          custom_colors=["blue", "blue"],
                           custom_linestyles=["solid"] * 3,
                           custom_linewidth=3,
                           arrow_colors=["black"],
@@ -134,7 +101,7 @@ EvT.plot_run_length_distr(buffer=0,
                           zero_distance=0.0,
                           ax=ax_array[1], figure=fig,
                           no_transform=True,
-                          date_instructions_formatter=None,     # yearsFmt,
+                          date_instructions_formatter=None,
                           date_instructions_locator=None,
                           ylabel_coords=ylabel_coords,
                           xlab="Year",
@@ -149,7 +116,7 @@ fig.savefig(os.path.join(baseline_working_directory, "Output", "ICML18_Figure5_N
 if os.path.isfile(os.path.join(baseline_working_directory, "Output", "ICML18_Figure5_Nile.pdf")):
     print("Figure 5 can be found in the Output directory")
 
-"""STEP 5: Generate additional figures, if requested"""
+"""STEP 4: Generate additional figures, if requested"""
 
 if save_extra_figures:
 
